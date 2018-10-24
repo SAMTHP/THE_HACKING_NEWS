e8f80e206cd45954d26d172e48c8ed076468f5e3
Vano <hervelee99@gmail.com>
2018-10-24 19:39:50 +0200

    ajout seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do
+  user = User.create!(name: Faker::Company.name)
+end
+10.times do
+  lien = Lien.create!(url: Faker::Company.name,user_id: rand(1..10))
+end
+10.times do
+  comments = Commentary.create!(content: Faker::Company.name, user_id: rand(1..10), lien_id: rand(1..10))
+end
+10.times do
+  user = CommentToCommentary.create!(content: Faker::Company.name, user_id: rand(1..10), commentary_id: rand(1..10))
+end
